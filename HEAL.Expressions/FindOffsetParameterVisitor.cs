b2f7f506489e0835440568c7da35a21b6f334c6e
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2023-02-20 18:43:07 +0100

    Added two files that I forgot to commit earlier.

---
+++ HEAL.Expressions/FindOffsetParameterVisitor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace HEAL.Expressions {
+  // finds the index of the offset parameter if it exists
+  internal class FindOffsetParameterVisitor : ExpressionVisitor {
+    private readonly ParameterExpression theta;
+    private int paramIdx = -1;
+
+    private FindOffsetParameterVisitor(ParameterExpression theta) {
+      this.theta = theta;
+    }
+
+    internal static int FindOffsetParameter(Expression expr, ParameterExpression theta) {
+      var v = new FindOffsetParameterVisitor(theta);
+      v.Visit(expr);
+      return v.paramIdx;
+    }
+
+    protected override Expression VisitBinary(BinaryExpression node) {
+      if (node.NodeType == ExpressionType.Add || node.NodeType == ExpressionType.Subtract) {
+        if (TryGetParameterIndex(node.Left, out var index) || TryGetParameterIndex(node.Right, out index)) {
+          this.paramIdx = index;
+          return node;
+        }
+
+        // both sides of Add/Sub can still contain the offset parameter
+        Visit(node.Left);
+        Visit(node.Right);
+      }
+      return node;
+    }
+
+    protected override Expression VisitUnary(UnaryExpression node) {
+      if (node.NodeType == ExpressionType.UnaryPlus || node.NodeType == ExpressionType.Negate) return Visit(node.Operand);
+      else return node;
+    }
+
+    private bool TryGetParameterIndex(Expression expr, out int index) {
+      if (expr is BinaryExpression binaryExpression && binaryExpression.NodeType == ExpressionType.ArrayIndex && binaryExpression.Left == theta) {
+        index = (int)((ConstantExpression)binaryExpression.Right).Value;
+        return true;
+      }
+      index = -1;
+      return false;
+    }
+  }
+}
---
+++ HEAL.Expressions/FindScalingParameterVisitor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace HEAL.Expressions {
+  // finds the index of the parameter which scales the whole expression if it exists
+  internal class FindScalingParameterVisitor : ExpressionVisitor {
+    private readonly ParameterExpression theta;
+    private int paramIdx = -1;
+
+    private FindScalingParameterVisitor(ParameterExpression theta) {
+      this.theta = theta;
+    }
+
+    internal static int FindScalingParameter(Expression expr, ParameterExpression theta) {
+      var v = new FindScalingParameterVisitor(theta);
+      v.Visit(expr);
+      return v.paramIdx;
+    }
+
+    protected override Expression VisitBinary(BinaryExpression node) {
+      if (node.NodeType == ExpressionType.Multiply) {
+        if (TryGetParameterIndex(node.Left, out var index) || TryGetParameterIndex(node.Right, out index)) {
+          this.paramIdx = index;
+          return node;
+        }
+
+        // both sides of multiply can still contain the scaling parameter
+        Visit(node.Left);
+        Visit(node.Right);
+      } else if (node.NodeType == ExpressionType.Divide) {
+        if (TryGetParameterIndex(node.Left, out var index)) {
+          this.paramIdx = index;
+          return node;
+        }
+        Visit(node.Left); // left can still contain a scaling parameter
+      }
+      return node;
+    }
+
+    protected override Expression VisitUnary(UnaryExpression node) {
+      if (node.NodeType == ExpressionType.UnaryPlus || node.NodeType == ExpressionType.Negate) return Visit(node.Operand);
+      else return node;
+    }
+
+    private bool TryGetParameterIndex(Expression expr, out int index) {
+      if(expr is BinaryExpression binaryExpression && binaryExpression.NodeType == ExpressionType.ArrayIndex && binaryExpression.Left == theta) {
+        index = (int) ((ConstantExpression)binaryExpression.Right).Value;
+        return true;
+      }
+      index = -1;
+      return false;
+    }
+  }
+}
