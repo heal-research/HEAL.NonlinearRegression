7d0143c3e382818ccf86b7fbd6d76c45397f6475
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2023-03-14 18:58:22 +0100

    Worked on residual standard error for Laplace approximation.

--- HEAL.NonlinearRegression/LaplaceApproximation.cs
+++ HEAL.NonlinearRegression/LaplaceApproximation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.ExceptionServices;
 
@@ -36,7 +37,6 @@ namespace HEAL.NonlinearRegression {
       var yPred = new double[m];
       var U = new double[n, n];
       negLogLikeHessian(pOpt, x, yPred, U); // Hessian is symmetric positive definite in pOpt
-      double[,] invH;
       try {
         alglib.spdmatrixcholesky(ref U, n, isupper: true);
         alglib.spdmatrixcholeskyinverse(ref U, n, isupper: true, out var info, out var rep, null); // calculates (U^T U) ^-1 = H^-1
@@ -94,51 +94,49 @@ namespace HEAL.NonlinearRegression {
 
       // TODO (similar to above)
 
-      // var yPred = new double[numRows];
-      // var J = new double[numRows, n];
-      // jacobian(paramEst, x, yPred, J); // jacobian for x
+      var yPred = new double[numRows];
+      var J = new double[numRows, n];
+      jacobian(paramEst, x, yPred, J); // jacobian for x
       // 
-      // // 1-alpha approximate inference interval for the expected response
+      // 1-alpha approximate inference interval for the expected response
       // // (1.37), page 23
-      // var J_invR = new double[numRows, n];
-      // alglib.rmatrixgemm(numRows, n, n, 1.0, J, 0, 0, 0, invR, 0, 0, 0, 0.0, ref J_invR, 0, 0); // Use invR from trainX because it captures the correlation
-      //                                                                                     // and uncertainty of parameters based on the training set.
-      // 
-      // // s * || J_invR ||
-      // for (int i = 0; i < numRows; i++) {
-      //   resStdError[i] = 0.0;
-      //   for (int j = 0; j < n; j++) {
-      //     resStdError[i] += J_invR[i, j] * J_invR[i, j];
-      //   }
-      //   resStdError[i] = Math.Sqrt(resStdError[i]); // length of row vector in JR
-      //   resStdError[i] *= s; // standard error for residuals  (determined on training set)
-      // }
+      // s * sqrt(J H^-1 J')
+
+      for (int i = 0; i < numRows; i++) {
+        resStdError[i] = 0.0;
+        for (int j = 0; j < n; j++) {
+          for (int k = 0; k < n; k++) {
+            resStdError[i] += J[i, k] * invH[j, k] * J[i, k];
+          }
+        }
+        resStdError[i] = s * Math.Sqrt(resStdError[i]);
+      }
       // 
       // // https://en.wikipedia.org/wiki/Confidence_and_prediction_bands
-      // var f = alglib.invfdistribution(n, this.m - n, alpha);
-      // var t = alglib.invstudenttdistribution(this.m - n, 1 - alpha / 2);
-      // 
-      // var noiseStdDev = includeNoise ? s : 0.0;
-      // 
-      // // Console.WriteLine($"noiseStdDev: {noiseStdDev} f: {f}, Math.Sqrt(n * f) {Math.Sqrt(n * f)} t: {t}");
-      // 
-      // //   // point-wise interval
-      // for (int i = 0; i < numRows; i++) {
-      //   low[i] = yPred[i] - (resStdError[i] + noiseStdDev) * t;
-      //   high[i] = yPred[i] + (resStdError[i] + noiseStdDev) * t;
+      var f = alglib.invfdistribution(n, this.m - n, alpha);
+      var t = alglib.invstudenttdistribution(this.m - n, 1 - alpha / 2);
+      
+      var noiseStdDev = includeNoise ? s : 0.0;
+      
+      // Console.WriteLine($"noiseStdDev: {noiseStdDev} f: {f}, Math.Sqrt(n * f) {Math.Sqrt(n * f)} t: {t}");
+      
+      //   // point-wise interval
+      for (int i = 0; i < numRows; i++) {
+        low[i] = yPred[i] - (resStdError[i] + noiseStdDev) * t;
+        high[i] = yPred[i] + (resStdError[i] + noiseStdDev) * t;
+      }
+      // old code to calculate pointwise and simultaneous intervals
+      // if (m == 1) {
+      //   // point-wise interval
+      //   low[0] = yPred[0] - (resStdError[0] + noiseStdDev) * t;
+      //   high[0] = yPred[0] + (resStdError[0] + noiseStdDev) * t;
+      // } else {
+      //   // simultaneous interval (band)
+      //   for (int i = 0; i < m; i++) {
+      //     low[i] = yPred[i] - (resStdError[i] + noiseStdDev)* Math.Sqrt(n * f); // not sure if t or sqrt(n*f) should be used for the noise part
+      //     high[i] = yPred[i] + (resStdError[i] + noiseStdDev) * Math.Sqrt(n * f);
+      //   }
       // }
-      // // old code to calculate pointwise and simultaneous intervals
-      // // if (m == 1) {
-      // //   // point-wise interval
-      // //   low[0] = yPred[0] - (resStdError[0] + noiseStdDev) * t;
-      // //   high[0] = yPred[0] + (resStdError[0] + noiseStdDev) * t;
-      // // } else {
-      // //   // simultaneous interval (band)
-      // //   for (int i = 0; i < m; i++) {
-      // //     low[i] = yPred[i] - (resStdError[i] + noiseStdDev)* Math.Sqrt(n * f); // not sure if t or sqrt(n*f) should be used for the noise part
-      // //     high[i] = yPred[i] + (resStdError[i] + noiseStdDev) * Math.Sqrt(n * f);
-      // //   }
-      // // }
     }
 
 
