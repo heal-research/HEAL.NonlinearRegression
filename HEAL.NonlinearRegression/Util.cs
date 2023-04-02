ed1510088ed7f0b04ab0eaa83bf8aa990a85f142
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2023-04-02 13:42:48 +0200

    Use alglib to calculate variance.

--- HEAL.NonlinearRegression/Util.cs
+++ HEAL.NonlinearRegression/Util.cs
@@ -5,13 +5,7 @@ namespace HEAL.NonlinearRegression {
   public static class Util {
 
     public static double Variance(double[] x) {
-      var xm = x.Average();
-      var SSR = 0.0;
-      for (int i = 0; i < x.Length; i++) {
-        var r = x[i] - xm;
-        SSR += r * r;
-      }
-      return SSR / x.Length;
+      return alglib.samplevariance(x) * (x.Length -1) / x.Length; // population variance
     }
 
     internal static void CopyRow(double[,] x, int rowIdx, double[] xi) {
