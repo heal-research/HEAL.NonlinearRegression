464f51c43d3097365a16298cb03d407e95d876c2
Gabriel Kronberger <gabriel.kronberger@heuristiclab.com>
2023-07-18 10:48:09 +0200

    Make sure OptReport is always set, even if the model has no parameters.

--- HEAL.NonlinearRegression/NonlinearRegression.cs
+++ HEAL.NonlinearRegression/NonlinearRegression.cs
@@ -68,7 +68,16 @@ namespace HEAL.NonlinearRegression {
 
     private void Fit(double[] p, int maxIterations = 0, double[] scale = null, double[] diagHess = null, double stepMax = 0.0, double epsF = 0.0, Func<double[], double, bool> callback = null) {
       int n = p.Length;
-      if (n == 0) return;
+      if (n == 0) {
+        OptReport = new OptimizationReport() {
+          Success = true,
+          Iterations = 0,
+          NumFuncEvals = 0,
+          NumJacEvals = 0
+        };
+
+        return;
+      }
 
 
       #region L-BFGS
